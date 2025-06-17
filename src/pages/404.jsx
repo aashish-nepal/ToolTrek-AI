2e704496d185f3262385b22aca0a5777c4932cb1
Aashish Nepal <nepal.aashish00@gmail.com>
2025-06-18 01:17:34 +0545

    startup

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return (
+      <div style={{ textAlign: 'center', padding: '4rem' }}>
+        <h1 style={{ fontSize: '2rem' }}>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
+    );
+  }
+  
